d241234af6493bd7a16e8feab85ac5f0837e7afd
poccariswet <s1240231@gmail.com>
2020-02-05 20:06:10 +0900

    [ADD] clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("image resizer")
+        .version("1.0")
+        .author("poccariswet <poccariswet2727@gmail.com>")
+        .about("Does awesome things")
+        .args_from_usage(
+            "-c, --config=[FILE] 'Sets a custom config file'
+                              <INPUT>              'Sets the input file to use'
+                              -v...                'Sets the level of verbosity'",
+        )
+        .subcommand(
+            SubCommand::with_name("test")
+                .about("controls testing features")
+                .version("1.3")
+                .author("Someone E. <someone_else@other.com>")
+                .arg("-d, --debug 'Print debug information'"),
+        )
+        .get_matches();
 }
