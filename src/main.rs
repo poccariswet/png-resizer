82e23a3ced28fd56b50e0d71152756bd92c15715
poccariswet <s1240231@gmail.com>
2020-02-05 22:02:57 +0900

    [ADD] init command line

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,50 @@
 extern crate clap;
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg};
 
 fn main() {
-    let matches = App::new("image resizer")
+    let matches = App::new("png-resizer")
         .version("1.0")
-        .author("poccariswet <poccariswet2727@gmail.com>")
-        .about("Does awesome things")
-        .args_from_usage(
-            "-c, --config=[FILE] 'Sets a custom config file'
-                              <INPUT>              'Sets the input file to use'
-                              -v...                'Sets the level of verbosity'",
+        .author("poccariswet <poccariswet@gmail.com>")
+        .about("png resize command line interface")
+        .arg(
+            Arg::with_name("width")
+                .short("w")
+                .long("width")
+                .value_name("NUM")
+                .help("Sets a width value")
+                .takes_value(true)
+                .required(true),
         )
-        .subcommand(
-            SubCommand::with_name("test")
-                .about("controls testing features")
-                .version("1.3")
-                .author("Someone E. <someone_else@other.com>")
-                .arg("-d, --debug 'Print debug information'"),
+        .arg(
+            Arg::with_name("height")
+                .short("h")
+                .long("hegiht")
+                .value_name("NUM")
+                .help("Sets a height value")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .value_name("FILES")
+                .help("Sets for resize files")
+                .index(1)
+                .multiple(true)
+                .required(true),
         )
         .get_matches();
+
+    if let Some(in_files) = matches.values_of("input") {
+        for file in in_files {
+            println!("An input file: {}", file);
+        }
+    }
+
+    if let Some(c) = matches.value_of("width") {
+        println!("Value for width: {}", c);
+    }
+
+    if let Some(c) = matches.value_of("height") {
+        println!("Value for height: {}", c);
+    }
 }
